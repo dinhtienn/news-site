7d7f5fbcdbe1602d324e27f7c90623f5a0fc3641
dinhtienn <dinhtiennguyen.1202@gmail.com>
2020-09-01 10:47:29 +0700

    Fix pull fail save

--- resources/views/components/sidebar_tags.blade.php
+++ resources/views/components/sidebar_tags.blade.php
@@ -1,6 +1,6 @@
 @if (isset($latestTags))
     <div class="tag_widget category-holder">
-        <div class="title-holder2"><h3>Tags</h3></div>
+        <div class="title-holder2"><h3>{{ config('view.tags') }}</h3></div>
 
         @foreach ($latestTags as $tag)
             <a href="{{ route('tag.detail', ['name' => $tag->name]) }}">{{ $tag->name }}</a>
